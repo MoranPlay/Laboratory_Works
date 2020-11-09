b2ddf8a8bd601077d7111e0cd6aa11a478949342
MoranPlay <kirilovskiy99@gmail.com>
2020-11-09 18:18:26 +0200

    da

--- Lab7.py
+++ Lab7.py
@@ -1,121 +1,96 @@
-import re
 from urllib.parse import urlparse
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 import urllib.request
 from tkinter import *
 from tkinter import filedialog as fd
-# root = Tk()
-# url_label = Label(root, text='Введите url адрес')
-# url_entry = Entry(root, width=50)
-#
-# links_label = Label(root, text='Введите глубину анализа ссылок')
-# links_entry = Entry(root, width=50)
-# open_file_button = Button(root, text='Выберите директорию')
-# start_button = Button(root, text='Стартуем!')
+from datetime import datetime
 
-# directory = ''
+root = Tk()
+
+time_label = Label(root, text='')
+url_label = Label(root, text='Введите url адрес')
+url_entry = Entry(root, width=50)
+
+links_label = Label(root, text='Введите глубину анализа ссылок')
+links_entry = Entry(root, width=50)
+open_file_button = Button(root, text='Выберите директорию')
+start_button = Button(root, text='Стартуем!')
+
+time_label['text'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+directory = ''
 url = 'http://www.antagene.com/en/pkd-polycystic-kidney-diseasebritish-shorthair'
-img = '/sites/all/themes/antagene/logo2_en.png'
-if (url.startswith('/')):
-    print(" url ok")
-else: print("url not ok")
-if (img.startswith('/')):
-    print(" url ok")
-else: print("url not ok")
 
-if (link.get('src').startswith('/')):
-        #     new_url = urljoin(url, link.get('src'))
+def openDirectory(event):
+    global directory
+    directory = fd.askdirectory()
+
+open_file_button.bind('<Button-1>', openDirectory)
+
+def get_image(soup, url):
+    for link in soup.find_all('img'):
+        webpage_url = url
+        src = link.get('src')
+        # if src.startswith('http://'):
+        #     resource = urllib.request.urlopen(src)
+        #     out = open(directory + "/" + src.split("/")[-1].split("?")[0], 'wb')
+        #     out.write(resource.read())
+        #     out.close()
+        #     return src, src.split("/")[-1].split("?")[0]
         # else:
-        #     if(link.get('src').startswith('http://')):
-        #         new_url = link.get('src')
-        #     else:
-        #         try:
-        #             link.get('src')
-        #         except :
-        #             print("Unknown url type")
+        new_url = urljoin(webpage_url, src)
+        resource = urllib.request.urlopen(new_url)
+        out = open(directory + "/" + new_url.split("/")[-1].split("?")[0], 'wb')
+        out.write(resource.read())
+        out.close()
+        return src, new_url.split("/")[-1].split("?")[0]
 
-# def openDirectory(event):
-#     global directory
-#     directory = fd.askdirectory()
-#
-# open_file_button.bind('<Button-1>', openDirectory)
-#
-# def get_image(soup, url):
-#     for link in soup.find_all('img'):
-#         webpage_url = url
-#         src = link.get('src')
-#         new_url = urljoin(webpage_url, src)
-#         resource = urllib.request.urlopen(new_url)
-#         out = open(directory + "/" + new_url.split("/")[-1].split("?")[0], 'wb')
-#         out.write(resource.read())
-#         out.close()
-#
-# def recursiveUrl(url, depth):
-#     if depth == 2:
-#         return url
-#     else:
-#         page = urllib.request.urlopen(url)
-#         soup = BeautifulSoup(page.read(), 'lxml')
-#         output = open(directory + '/' + urlparse(url).hostname + '.html', 'w', encoding='utf-8')
-#         output.write(str(soup))
-#         get_image(soup, url)
-#         newlink = soup.findAll('a', attrs={'href': re.compile("http://")}, limit=3)
-#         if len(newlink) == 0:
-#             return url
-#         else:
-#             for link in newlink:
-#                 return recursiveUrl(link.get('href'), depth + 1)
-#
-#
-# def getLinks(url):
-#     links = [url]
-#     page = urllib.request.urlopen(url)
-#     soup = BeautifulSoup(page.read(), 'lxml')
-#     output = open(directory + '/' + urlparse(url).hostname + '.html', 'w', encoding='utf-8')
-#     output.write(str(soup))
-#     get_image(soup, url)
-#     for link in soup.findAll('a', attrs={'href': re.compile("http://")}, limit=3):
-#         links.append(recursiveUrl(link.get('href'), 0))
-#     return links
-#
-# def start(event):
-#     print(getLinks(url))
-#
-# start_button.bind('<Button-1>', start)
-#
-# url_label.pack()
-# url_entry.pack()
-# links_label.pack()
-# links_entry.pack()
-# open_file_button.pack()
-# start_button.pack()
-# root.mainloop()
-
-        def get_image(soup, url):
-            # global new_url
-            for link in soup.find_all('img'):
-
-                src = link.get('src')
-                if (src.startswith('/')):
-                    new_url = urljoin(url, src)
-                    resource = urllib.request.urlopen(new_url)
-                    out = open(directory + "/" + new_url.split("/")[-1].split("?")[0], 'wb')
-                    out.write(resource.read())
-                    out.close()
+def recursiveUrl(url, depth):
+    if depth == 2:
+        return url
+    else:
+        page = urllib.request.urlopen(url)
+        soup = BeautifulSoup(page.read(), 'lxml')
+        links_image = get_image(soup, url)
+        output = open(directory + '/' + urlparse(url).hostname + '.html', 'w', encoding='utf-8')
+        output.write(str(soup))
+        newlink = soup.findAll('a', attrs={'href': re.compile("")}, limit=5)
+        if len(newlink) == 0:
+            return url
+        else:
+            for link in newlink:
+                new_url_link = link.get('href')
+                if new_url_link.startswith('http://'):
+                    return recursiveUrl(new_url_link, depth + 1)
                 else:
-                    if (src.startswith('http://')):
-                        new_url = src
-                        resource = urllib.request.urlopen(new_url)
-                        out = open(directory + "/" + new_url.split("/")[-1].split("?")[0], 'wb')
-                        out.write(resource.read())
-                        out.close()
-                    else:
-                        try:
-                            link.get('src')
-                        except ValueError:
-                            print("Unknown url type")
-                # resource = urllib.request.urlopen(new_url)
-                # out = open(directory + "/" + new_url.split("/")[-1].split("?")[0], 'wb')
-                # out.write(resource.read())
-                # out.close()
+                    return recursiveUrl(urljoin(url, new_url_link), depth + 1)
+
+
+def getLinks(url):
+    links = [url]
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page.read(), 'lxml')
+    links_image = get_image(soup, url)
+    output = open(directory + '/' + urlparse(url).hostname + '.html', 'w', encoding='utf-8')
+    output.write(str(soup).replace(links_image[0], links_image[1]))
+    for link in soup.findAll('a', attrs={'href': re.compile("")}, limit=5):
+        new_url_link = link.get('href')
+        if new_url_link.startswith('http://'):
+            links.append(recursiveUrl(new_url_link, 0))
+        else:
+            links.append(recursiveUrl(urljoin(url, new_url_link), 0))
+    return links
+
+def start(event):
+    print(getLinks(url))
+    exit()
+start_button.bind('<Button-1>', start)
+
+time_label.pack()
+url_label.pack()
+url_entry.pack()
+links_label.pack()
+links_entry.pack()
+open_file_button.pack()
+start_button.pack()
+root.mainloop()
--- sdc.py
+++ sdc.py
@@ -31,28 +31,28 @@ def get_image(soup, url):
     for link in soup.find_all('img'):
         webpage_url = url
         src = link.get('src')
-        if(src.startswith('http://')):
-            resource = urllib.request.urlopen(src)
-            out = open(directory + "/" + src.split("/")[-1].split("?")[0], 'wb')
-            out.write(resource.read())
-            out.close()
-        else:
-            new_url = urljoin(webpage_url, src)
-            resource = urllib.request.urlopen(new_url)
-            out = open(directory + "/" + new_url.split("/")[-1].split("?")[0], 'wb')
-            out.write(resource.read())
-            out.close()
+        new_url = urljoin(webpage_url, src)
+        resource = urllib.request.urlopen(new_url)
+        out = open(directory + "/" + new_url.split("/")[-1].split("?")[0], 'wb')
+        out.write(resource.read())
+        out.close()
+        # soup = BeautifulSoup(str(soup).replace(src, new_url.split("/")[-1].split("?")[0]), 'lxml')
+        soup = str(soup).replace(src, new_url.split("/")[-1].split("?")[0])
+    return str(soup)
+        #return src, new_url.split("/")[-1].split("?")[0]
 
 def recursiveUrl(url, depth):
-    if depth == 2:
+    if depth == 3:
         return url
     else:
         page = urllib.request.urlopen(url)
         soup = BeautifulSoup(page.read(), 'lxml')
+        #soup = get_image(soup, url)
+        #links_image = get_image(soup, url)
+        #print(links_image)
         output = open(directory + '/' + urlparse(url).hostname + '.html', 'w', encoding='utf-8')
-        output.write(str(soup))
-        get_image(soup, url)
-        newlink = soup.findAll('a', attrs={'href': re.compile("")}, limit=3)
+        output.write(get_image(soup, url))
+        newlink = soup.findAll('a', attrs={'href': re.compile("")}, limit=5)
         if len(newlink) == 0:
             return url
         else:
@@ -68,15 +68,17 @@ def getLinks(url):
     links = [url]
     page = urllib.request.urlopen(url)
     soup = BeautifulSoup(page.read(), 'lxml')
+    get_image(soup, url)
+    #soup = get_image(soup, url)
     output = open(directory + '/' + urlparse(url).hostname + '.html', 'w', encoding='utf-8')
     output.write(str(soup))
-    get_image(soup, url)
-    for link in soup.findAll('a', attrs={'href': re.compile("")}, limit=5):
+    for link in soup.findAll('a', attrs={'href': re.compile(urlparse(url).hostname + "/")}, limit=5):
         new_url_link = link.get('href')
-        if new_url_link.startswith('http://'):
-            links.append(recursiveUrl(new_url_link, 0))
-        else:
-            links.append(recursiveUrl(urljoin(url, new_url_link), 0))
+        links.append(recursiveUrl(new_url_link, 0))
+        # if new_url_link.startswith('http://'):
+        #     links.append(recursiveUrl(new_url_link, 0))
+        # else:
+        #     links.append(recursiveUrl(urljoin(url, new_url_link), 0))
     return links
 
 def start(event):
