9817654ab028e79d73b26e7f1629d1ca62bd839c
MoranPlay <kirilovskiy99@gmail.com>
2020-12-07 11:36:02 +0200

    new1

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import math
 from tkinter import *
 from tkinter import filedialog as fd
 import matplotlib.pyplot as plt
+import random
+from pandas import DataFrame
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 root = Tk()
 path = ''
@@ -31,11 +34,11 @@ def drawGraphics(x, y, title):
     plt.xticks(x)
     plt.bar(x, y)
     plt.title(title)
-    plt.figure(2)
-    plt.show()
+    plt.figure(1)
 
 
 def calculateEntropy(event):
+    # Второй критерий рисуется в методе calculateEntropy()
     zeros = 0
     ones = 0
     lenf = 0
@@ -50,7 +53,6 @@ def calculateEntropy(event):
             ones += byte.count('1')
             lenf += 8
             byte = f.read(1)
-    #print(y1)
     entropy = -(zeros / lenf) * math.log2(zeros / lenf) - (ones / lenf) * math.log2(ones / lenf)
     entropy_label['text'] = f"H(A) = {entropy}"
     print(entropy)
@@ -59,12 +61,18 @@ def calculateEntropy(event):
     y = [zeros, ones]
     x = (0, 1)
     title = "Count bits"
+    S = []
+    for bits in y1:
+        for bit in bits:
+            S.append(int(bit))
     drawGraphics(x, y, title)
-    monotoneCheck(y1)
-
+    monotoneCheck(S)
+    minAAKF(S)
+    plt.show()
 
 
 start_button.bind('<Button-1>', calculateEntropy)
+
 
 def monotoneCheck(y1):
     vozr = 0
@@ -90,10 +98,41 @@ def monotoneCheck(y1):
             ub += 1
         elif y1[i] == y1[i - 1] and ub == 0:
             vozr += 1
-    plt.figure(1)
+    plt.figure(2)
+    plt.title("Check for monotony")
     plt.hist(t)
-    plt.show()
+    # plt.show()
 
+def minAAKF(y1):
+    S = y1
+    # for bits in y1:
+    #     for bit in bits:
+    #         S.append(int(bit))
+    print(S)
+    S1 = S + S
+
+    A = []
+    for i in range(len(S) - 1):
+        s = S1[i:i + len(S)]
+        a = [(-1) ** S[j] * (-1) ** s[j] for j in range(len(S))]
+        A.append(sum(a) / len(S))
+
+    x = list(range(len(A)))
+
+    Data2 = {'Index': x,
+             'AAKF': A}
+
+    df2 = DataFrame(Data2, columns=['Index', 'AAKF'])
+    df2 = df2[['Index', 'AAKF']].groupby('Index').sum()
+
+    window = Tk()
+    figure3 = plt.Figure(figsize=(5,4), dpi=100)
+    ax2 = figure3.add_subplot(111)
+    line2 = FigureCanvasTkAgg(figure3, window)
+    line2.get_tk_widget().pack()
+    df2.plot(kind='line', legend=True, ax=ax2, color='r', marker='o', fontsize=10)
+    ax2.set_title('AAKF')
+    #window.mainloop()
 
 
 root.mainloop()
